aea1a38b8f93e9246db30244813376ab2e13d7b6
Pratik <pratikkumar@gmail.com>
2021-10-11 02:45:52 +0530

    codechef questions

---
+++ chopsticks1.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, diff;
+    cin >> n;
+    cin >> diff;
+    int L[n];
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> L[i];
+    }
+
+    for (int i = 0; i < n - 1;)
+    {
+
+        if (L[i] >= L[i + 1] - diff)
+        {
+
+            count++;
+            i += 2;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    cout << count << endl;
+
+    return 0;
+}
